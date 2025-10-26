cb3825c0ce0c38f600b8cd5e141e43389d8679b3
346dabf2-85e7-4e8c-8f75-adec844792a6 <riccardo.tiebax@cz.nl>
2025-10-26 13:35:03 +0000

    fix(models): fixed ReportingBalanceByClassification controller

--- src/exactpy/controllers/reporting_balance_by_classification.py
+++ src/exactpy/controllers/reporting_balance_by_classification.py
@@ -2,8 +2,8 @@ from exactpy.controllers import BaseController
 from exactpy.models.account import Account
 
 
-class AccountController(BaseController):
-    _resource = "crm/Accounts"
+class ReportingBalanceByClassificationController(BaseController):
+    _resource = "read/financial/ReportingBalanceByClassification"
     _mandatory_query_arg_options = ["gl_scheme", "reporting_year"]
     _mandatory_filter_options = []
     _model = Account
