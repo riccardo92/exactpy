15875873fab389be99c09fb9f220974eb69c9074
346dabf2-85e7-4e8c-8f75-adec844792a6 <riccardo.tiebax@cz.nl>
2025-10-27 20:52:35 +0000

    fix(models/base.py): enum value serializaton

--- src/exactpy/models/base.py
+++ src/exactpy/models/base.py
@@ -7,4 +7,5 @@ class ExactOnlineBaseModel(BaseModel):
     model_config = ConfigDict(
         alias_generator=AliasGenerator(alias=special_snake_to_pascal),
         validate_by_alias=True,
+        use_enum_values=True,
     )
