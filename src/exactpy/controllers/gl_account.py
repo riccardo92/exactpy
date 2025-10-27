6f94d20969d5927c18931b537dd073e5232e2609
346dabf2-85e7-4e8c-8f75-adec844792a6 <riccardo.tiebax@cz.nl>
2025-10-27 12:47:43 +0000

    fix(controllers): added GLAccount controller

---
+++ src/exactpy/controllers/gl_account.py
@@ -0,0 +1,9 @@
+from exactpy.controllers import BaseController
+from exactpy.models.gl_account import GLAccount
+
+
+class GLAccountController(BaseController):
+    _resource = "financial/GLAccounts"
+    _mandatory_query_arg_options = []
+    _mandatory_filter_options = []
+    _model = GLAccount
