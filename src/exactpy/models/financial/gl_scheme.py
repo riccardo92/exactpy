665d47b920e1685dc4f220624d464b01f8dc1d26
346dabf2-85e7-4e8c-8f75-adec844792a6 <riccardo.tiebax@cz.nl>
2025-10-28 09:57:08 +0000

    fix(models): implemented GLScheme model

---
+++ src/exactpy/models/financial/gl_scheme.py
@@ -0,0 +1,20 @@
+from exactpy.models.base import ExactOnlineBaseModel
+from exactpy.types import (
+    GUID,
+    ExactUnixTimestamp,
+)
+
+
+class GLSchemeModel(ExactOnlineBaseModel):
+    id: GUID
+    code: str | None = None
+    created: ExactUnixTimestamp
+    creator: GUID
+    creator_full_name: str | None = None
+    description: str | None = None
+    division: int | None = None
+    main: int | None = None
+    modified: ExactUnixTimestamp
+    modifier: GUID
+    modifier_full_name: str | None = None
+    target_namespace: str | None = None
