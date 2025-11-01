cd40e33200b838835e406294622e8e39410c0eaa
346dabf2-85e7-4e8c-8f75-adec844792a6 <riccardo.tiebax@cz.nl>
2025-11-01 12:08:34 +0000

    fix(models): added spark fields aging overview model

--- src/exactpy/models/financial/aging_overview.py
+++ src/exactpy/models/financial/aging_overview.py
@@ -1,10 +1,12 @@
+from sparkdantic import SparkField
+
 from exactpy.models.base import ExactOnlineBaseModel
 
 
 class AgingOverviewModel(ExactOnlineBaseModel):
     _pk = "age_group"
-    age_group: int | None = None
-    age_group_description: str | None = None
-    amount_payable: float | None = None
-    amount_receivable: float | None = None
-    currency_code: str | None = None
+    age_group: int | None = SparkField(spark_type="integer", default=None)
+    age_group_description: str | None = SparkField(spark_type="string", default=None)
+    amount_payable: float | None = SparkField(spark_type="double", default=None)
+    amount_receivable: float | None = SparkField(spark_type="double", default=None)
+    currency_code: str | None = SparkField(spark_type="string", default=None)
