409599a004a2f14b062e6bfce41e9c4a240b0cb6
346dabf2-85e7-4e8c-8f75-adec844792a6 <riccardo.tiebax@cz.nl>
2025-11-01 20:01:45 +0000

    fix(models): added spark fields aging paybles receivables list model

--- src/exactpy/models/financial/aging_payables_receivables_list.py
+++ src/exactpy/models/financial/aging_payables_receivables_list.py
@@ -1,3 +1,5 @@
+from sparkdantic import SparkField
+
 from exactpy.models.base import ExactOnlineBaseModel
 from exactpy.types import (
     GUID,
@@ -6,20 +8,20 @@ from exactpy.types import (
 
 class AgingPayablesReceivablesListModel(ExactOnlineBaseModel):
     _pk = "account_id"
-    account_id: GUID
-    account_code: str | None = None
-    account_name: str | None = None
-    age_group1: int | None = None
-    age_group1_amount: int | None = None
-    age_group1_description: str | None = None
-    age_group2: int | None = None
-    age_group2_amount: int | None = None
-    age_group2_description: str | None = None
-    age_group3: int | None = None
-    age_group3_amount: int | None = None
-    age_group3_description: str | None = None
-    age_group4: int | None = None
-    age_group4_amount: float | None = None
-    age_group4_description: str | None = None
-    currency_code: str | None = None
-    total_amount: int | None = None
+    account_id: GUID = SparkField(spark_type="string")
+    account_code: str | None = SparkField(spark_type="string", default=None)
+    account_name: str | None = SparkField(spark_type="string", default=None)
+    age_group1: int | None = SparkField(spark_type="integer", default=None)
+    age_group1_amount: int | None = SparkField(spark_type="integer", default=None)
+    age_group1_description: str | None = SparkField(spark_type="string", default=None)
+    age_group2: int | None = SparkField(spark_type="integer", default=None)
+    age_group2_amount: int | None = SparkField(spark_type="integer", default=None)
+    age_group2_description: str | None = SparkField(spark_type="string", default=None)
+    age_group3: int | None = SparkField(spark_type="integer", default=None)
+    age_group3_amount: int | None = SparkField(spark_type="integer", default=None)
+    age_group3_description: str | None = SparkField(spark_type="string", default=None)
+    age_group4: int | None = SparkField(spark_type="integer", default=None)
+    age_group4_amount: float | None = SparkField(spark_type="float", default=None)
+    age_group4_description: str | None = SparkField(spark_type="string", default=None)
+    currency_code: str | None = SparkField(spark_type="string", default=None)
+    total_amount: int | None = SparkField(spark_type="integer", default=None)
