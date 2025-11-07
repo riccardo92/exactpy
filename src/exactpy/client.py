d6211948907136d32acc4ebcd8ba7363b55c1419
346dabf2-85e7-4e8c-8f75-adec844792a6 <riccardo.tiebax@cz.nl>
2025-11-07 21:18:33 +0000

    fix(client.py): refactored namespace and controller structure, now with static properties

--- src/exactpy/client.py
+++ src/exactpy/client.py
@@ -8,49 +8,10 @@ import httpx
 from loguru import logger
 
 from exactpy.auth import Auth
-from exactpy.controllers.crm import (
-    AccountController,
-)
-from exactpy.controllers.financial import (
-    AgingOverviewByAccountController,
-    AgingOverviewController,
-    AgingPayablesListByAgeGroupController,
-    AgingPayablesListController,
-    AgingReceivablesListByAgeGroupController,
-    AgingReceivablesListController,
-    DeductibilityPercentageController,
-    ExchangeRateController,
-    FinancialPeriodController,
-    GLAccountClassificationMappingsController,
-    GLAccountController,
-    GLSchemeController,
-    GLTransactionSourceController,
-    GLTransactionTypeController,
-    JournalController,
-    JournalStatusListController,
-    OfficialReturnController,
-    OustandingInvoicesOverviewController,
-    PayablesListByAccountAndAgeGroupController,
-    PayablesListByAccountController,
-    PayablesListByAgeGroupController,
-    PayablesListController,
-    ProfitLossOverviewController,
-    ReceivablesListByAccountAndAgeGroupController,
-    ReceivablesListByAccountController,
-    ReceivablesListByAgeGroupController,
-    ReceivablesListController,
-    ReportingBalanceByClassificationController,
-    ReportingBalanceController,
-    ReturnController,
-    RevenueListByYearAndStatusController,
-    RevenueListByYearController,
-    RevenueListController,
-)
-from exactpy.controllers.system import (
-    DivisionController,
-    MeController,
-)
 from exactpy.exceptions import DailyLimitExceededException, NoDivisionSetException
+from exactpy.namespaces.crm import CRMNamespace
+from exactpy.namespaces.financial import FinancialNamespace
+from exactpy.namespaces.system import SystemNamespace
 from exactpy.types import FilterOperatorEnum
 
 BASE_HEADERS = {"Content-Type": "application/json", "Accept": "application/json"}
@@ -60,6 +21,10 @@ class Namespace: ...
 
 
 class Client:
+    crm: CRMNamespace
+    financial: FinancialNamespace
+    system: SystemNamespace
+
     def __init__(
         self,
         client_id: str,
@@ -458,71 +423,6 @@ class Client:
     def _setup_namespaces_and_controllers(self):
         """Register namespaces and initialize controller classes."""
         # Set up namespaces
-        self.financial = Namespace()
-        self.crm = Namespace()
-        self.system = Namespace()
-
-        # Set up endpoints
-        self.financial.aging_overviews = AgingOverviewController(self)
-        self.financial.aging_overviews_by_account = AgingOverviewByAccountController(
-            self
-        )
-        self.financial.aging_payables_lists = AgingPayablesListController(self)
-        self.financial.aging_payables_lists_by_age = (
-            AgingPayablesListByAgeGroupController(self)
-        )
-        self.financial.aging_receivables_lists = AgingReceivablesListController(self)
-        self.financial.aging_receivables_lists_by_age = (
-            AgingReceivablesListByAgeGroupController(self)
-        )
-        self.financial.deductibilities = DeductibilityPercentageController(self)
-        self.financial.exchange_rates = ExchangeRateController(self)
-        self.financial.financial_periods = FinancialPeriodController(self)
-        self.financial.gl_accounts_classification_mappings = (
-            GLAccountClassificationMappingsController(self)
-        )
-        self.financial.gl_accounts = GLAccountController(self)
-        self.financial.gl_schemes = GLSchemeController(self)
-        self.financial.gl_transaction_sources = GLTransactionSourceController(self)
-        self.financial.gl_transaction_types = GLTransactionTypeController(self)
-
-        self.financial.journal_status_lists = JournalStatusListController(self)
-        self.financial.journals = JournalController(self)
-        self.financial.official_returns = OfficialReturnController(self)
-        self.financial.outstanding_invoices_overviews = (
-            OustandingInvoicesOverviewController(self)
-        )
-        self.financial.payables_lists = PayablesListController(self)
-        self.financial.payables_lists_by_account = PayablesListByAccountController(self)
-        self.financial.payables_lists_by_age_group = PayablesListByAgeGroupController(
-            self
-        )
-        self.financial.payables_lists_by_account_and_age_group = (
-            PayablesListByAccountAndAgeGroupController(self)
-        )
-
-        self.financial.receivables_lists = ReceivablesListController(self)
-        self.financial.receivables_lists_by_account = (
-            ReceivablesListByAgeGroupController(self)
-        )
-        self.financial.receivables_lists_by_age_group = (
-            ReceivablesListByAccountController(self)
-        )
-        self.financial.receivables_lists_by_account_and_age_group = (
-            ReceivablesListByAccountAndAgeGroupController(self)
-        )
-        self.financial.profit_loss_overviews = ProfitLossOverviewController(self)
-        self.financial.reporting_balances = ReportingBalanceController(self)
-        self.financial.reporting_balances_by_classification = (
-            ReportingBalanceByClassificationController(self)
-        )
-        self.financial.returns = ReturnController(self)
-        self.financial.revenue_lists = RevenueListController(self)
-        self.financial.revenue_lists_by_year = RevenueListByYearController(self)
-        self.financial.revenue_lists_by_year_and_status = (
-            RevenueListByYearAndStatusController(self)
-        )
-
-        self.crm.accounts = AccountController(self)
-        self.system.me = MeController(self)
-        self.system.divisions = DivisionController(self)
+        self.financial = FinancialNamespace(self)
+        self.crm = CRMNamespace(self)
+        self.system = SystemNamespace(self)
