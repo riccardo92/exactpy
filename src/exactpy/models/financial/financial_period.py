eb028cdc2634985c5b24446726274bf446535984
346dabf2-85e7-4e8c-8f75-adec844792a6 <riccardo.tiebax@cz.nl>
2025-11-19 12:30:33 +0000

    fix(models): replaced wrong FinancialPeriod model attributes

--- src/exactpy/models/financial/financial_period.py
+++ src/exactpy/models/financial/financial_period.py
@@ -11,16 +11,10 @@ class FinancialPeriodModel(ExactOnlineBaseModel):
     creator: GUID = SparkField(spark_type="string")
     creator_full_name: str | None = SparkField(spark_type="string", default=None)
     division: int | None = SparkField(spark_type="integer", default=None)
+    end_date: ExactUnixTimestamp = SparkField(spark_type="timestamp")
+    fin_period: int | None = SparkField(spark_type="integer", default=None)
+    fin_year: int | None = SparkField(spark_type="integer", default=None)
     modified: ExactUnixTimestamp = SparkField(spark_type="timestamp")
     modifier: GUID = SparkField(spark_type="string")
     modifier_full_name: str | None = SparkField(spark_type="string", default=None)
-    rate: float | None = SparkField(spark_type="float", default=None)
-    source_currency: str | None = SparkField(spark_type="string", default=None)
-    source_currency_description: str | None = SparkField(
-        spark_type="string", default=None
-    )
     start_date: ExactUnixTimestamp = SparkField(spark_type="timestamp")
-    target_currency: str | None = SparkField(spark_type="string", default=None)
-    target_currency_description: str | None = SparkField(
-        spark_type="string", default=None
-    )
